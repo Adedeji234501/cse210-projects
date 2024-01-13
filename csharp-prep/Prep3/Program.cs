defb8d867adfcb47a6d5f65caf675fd619614f3f
Adedeji234501 <Azeezadedeji1k@gmail.com>
2024-01-12 23:40:07 -0800

    week 01 complete  hw

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        do
+        {
+            //Core Requirement 1: Ask for the  magic number
+            Console.WriteLine("Welcome to Guess My Number! ");
+
+            Console.Write("What is the magic number? ");
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            //Core Requirement 2: Add a loop for the game
+            int guess;
+            int guessCount = 0;
+        
+            do
+            {
+                //Core Requirement 3: Ask for a guess
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                //Core Requirement 4: Check if the guess is higher, lower, or correct
+                if (guess < magicNumber)
+                    Console.WriteLine("Higher");
+                else if (guess > magicNumber)
+                    Console.WriteLine("Lower");
+                else
+                    Console.WriteLine("Correct! You guessed right. ");
+
+            } while (guess != magicNumber);
+
+            //Stretch Chaallenge: Inform the user of the number of guesses
+            Console.WriteLine($"You guessed the correct number in {guessCount} {(guessCount == 1 ? "guess" : "guesses")}.");
+            
+            //Stretch Challenge: Ask if the user want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+
+        } while (Console.ReadLine().ToLower() == "yes");
     }
 }

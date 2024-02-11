90cc90ea885e9302003ccc20aa23818cc7b9234d
Adedeji234501 <Azeezadedeji1k@gmail.com>
2024-02-11 03:04:06 -0500

    wk05 hw

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,32 @@
+// Base class for goals
+
+public class Goal
+{
+    public string Name { get; set; }
+    public bool Completed { get; set; }
+
+    // Constructor
+    public Goal(string name)
+    {
+        Name = name;
+        Completed = false;
+    }
+
+    // Method to mark the goal as completed
+    public virtual void MarkCompleted()
+    {
+        Completed = true;
+    }
+
+    // Method to get the reward for completing the goal
+    public virtual int GetReward()
+    {
+        return 0;
+    }
+
+    public virtual void RecordProgress()
+    {
+       Completed = true;
+       Console.WriteLine($"You made progress on the {Name} goal.");
+    }
+}
